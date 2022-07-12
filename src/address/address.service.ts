e80bdb470523a744a8c3a17a9e0087f0d94a89b9
lucasgalarce <galarcelucas@gmail.com>
2022-07-12 11:04:48 -0300

    remove unnecessary code

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,16 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Web3Service } from '../web3/web3.service';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
-
-import { Address } from './address.interface';
 
 @Injectable()
 export class AddressService {
-  constructor(
-    private web3Service: Web3Service,
-    @InjectModel('Address') private addressModel: Model<Address>,
-  ) {}
+  constructor(private web3Service: Web3Service) {}
 
   async getBalance(address: string): Promise<string> {
     return await this.web3Service.getBalance(address);
